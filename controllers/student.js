90835e2d9a24cd5fc2d00e5593b146117e3954d7
kameswar24 <kameswarreddy@byteroar.in>
2022-01-20 19:07:06 +0530

    student

--- controllers/student.js
+++ controllers/student.js
@@ -36,7 +36,7 @@ exports.StudentDataUpdate = async (req, res) => {
 }
 exports.StudentLogin = async (req,res)=>{
     try {
-        var userName=req.body.userName
+        var { userName,password } = req.body
    await Student.findOne({$or:[{'email':userName}, {'phone':userName}]}).exec((err, suc) => {
           if(err){
             return res.json({
